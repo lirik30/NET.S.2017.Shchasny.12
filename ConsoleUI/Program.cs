343778b72e661224a0ee4b1b8eeab4f328cd200f
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-24 19:31:28 +0300

    Timer class and console ui added

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using TimerLogic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Timer = TimerLogic.Timer;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+            var alarm = new Alarm();
+            var calendar = new Calendar();
+            timer.Interval = 5000;
+            alarm.Register(timer);
+            calendar.Register(timer);
+            timer.Start("Hey!", "Get up!");
+            //alarm.Unregister(timer);
+            //calendar.Unregister(timer);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Alarm
+    {
+        public void Register(Timer timer) => timer.TimePassed += AlarmMsg;
+        public void Unregister(Timer timer) => timer.TimePassed -= AlarmMsg;
+
+        private void AlarmMsg(object sender, TimePassedEventArgs eventArgs)
+        {
+            Console.WriteLine("Time passed!");
+            Console.WriteLine($"{eventArgs.Title}{Environment.NewLine}{eventArgs.Subject}");
+            PlayMusic();
+        }
+
+        private void PlayMusic()
+        {
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(783, 300);
+            Console.Beep(523, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(261, 300);
+            Console.Beep(293, 300);
+            Console.Beep(329, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Thread.Sleep(300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(587, 300);
+            Thread.Sleep(300);
+            Console.Beep(783, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(659, 300);
+            Console.Beep(783, 300);
+            Console.Beep(523, 300);
+            Console.Beep(587, 300);
+            Console.Beep(659, 300);
+            Console.Beep(261, 300);
+            Console.Beep(293, 300);
+            Console.Beep(329, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Console.Beep(698, 300);
+            Thread.Sleep(300);
+            Console.Beep(698, 300);
+            Console.Beep(659, 300);
+            Console.Beep(659, 300);
+            Thread.Sleep(300);
+            Console.Beep(783, 300);
+            Console.Beep(783, 300);
+            Console.Beep(698, 300);
+            Console.Beep(587, 300);
+            Console.Beep(523, 600);
+            Thread.Sleep(600);
+        }
+    }
+
+    class Calendar
+    {
+        public void Register(Timer timer) => timer.TimePassed += CalendarMsg;
+        public void Unregister(Timer timer) => timer.TimePassed -= CalendarMsg;
+
+        private void CalendarMsg(object sender, TimePassedEventArgs eventArgs)
+        {
+            Console.WriteLine("It's calendar!");
+            Console.WriteLine($"{eventArgs.Title}{Environment.NewLine}{eventArgs.Subject}");
+            PlayMusic();
+        }
+
+        private void PlayMusic()
+        {
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(554, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(523, 1200);
+            Thread.Sleep(150);
+            Console.Beep(466, 150);
+            Console.Beep(523, 150);
+        }
+    }
+}
---
+++ TimerLogic/Timer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TimerLogic
+{
+    /// <summary>
+    /// Imitation of timer
+    /// </summary>
+    public class Timer
+    {
+        private long _interval;
+
+        /// <summary>
+        /// Time in milliseconds
+        /// </summary>
+        public long Interval
+        {
+            get => _interval;
+            set => _interval = value >= 0 ? value : throw new ArgumentOutOfRangeException();
+        }
+
+        /// <summary>
+        /// Contains events that will happen when time will passed
+        /// </summary>
+        public event EventHandler<TimePassedEventArgs> TimePassed = delegate { };
+
+        #region ctors
+
+        public Timer() : this(0) {}
+
+        public Timer(long interval) => Interval = interval;
+        #endregion
+        
+
+        protected virtual void OnTimePassed(TimePassedEventArgs eventArgs)
+        {
+            EventHandler<TimePassedEventArgs> temp = TimePassed;
+            temp?.Invoke(this, eventArgs);
+        }
+
+
+        /// <summary>
+        /// Start the timer
+        /// </summary>
+        /// <param name="title">Title of message</param>
+        /// <param name="subject">Subject of message</param>
+        public void Start(string title, string subject)
+        {
+            var s = Stopwatch.StartNew();//??????
+            Thread thread = new Thread(() =>
+            {
+                while (s.ElapsedMilliseconds != Interval) { }
+                OnTimePassed(new TimePassedEventArgs(title, subject));
+            });  
+            thread.Start();
+
+            
+        }
+    }
+
+    #region TimePassedEventArgs
+    /// <summary>
+    /// Keep information about the TimePassed event
+    /// </summary>
+    public class TimePassedEventArgs : EventArgs
+    {
+        public string Title { get; }
+        public string Subject { get; }
+
+        public TimePassedEventArgs(string title, string subject)
+        {
+            Title = title;
+            Subject = subject;
+        }
+    }
+    #endregion
+}
