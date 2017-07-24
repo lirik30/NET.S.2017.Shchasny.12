2bb29c86ae318b7bed9b492dd1a1f3f4c52e07b8
Kiril Shchasny <kiril3467@yandex.ru>
2017-07-24 20:49:15 +0300

    Generic algorithm of binary search added, covered by tests

---
+++ BinarySearchLogic.Tests/GenericBinaryLogicTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using static BinarySearchLogic.GenericBinarySearch;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace BinarySearchLogic.Tests
+{
+    [TestFixture]
+    public class GenericBinaryLogicTests
+    {
+        private static IEnumerable<TestCaseData> BinarySearch_PositiveData
+        {
+            get {
+                yield return new TestCaseData(
+                        new[] { "a", "bd", "cdd", "mno", "xxx" },
+                        "xxx").
+                    Returns(4);
+
+                yield return new TestCaseData(
+                        new[] { 1.0, 15.4, 16.6, 20.1, 44.4 },
+                        1.0).
+                    Returns(0);
+                
+                yield return new TestCaseData(
+                        new[] { 15, 22, 46, 70, 95 },
+                        70).
+                    Returns(3);
+
+                yield return new TestCaseData(
+                        new[] { 'a', 'c', 'k', 'm', 'x' },
+                        'c').
+                    Returns(1);
+
+
+                yield return new TestCaseData(
+                        new[] { 'a', 'c', 'k', 'm', 'x' },
+                        'f').
+                    Returns(-1);
+            }
+        }
+
+
+        private static IEnumerable<TestCaseData> BinarySearch_ThrowsRuntimeBinderExceptionData
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    't',
+                    'f');
+
+                yield return new TestCaseData(
+                    1,
+                    4);
+
+                yield return new TestCaseData(
+                    new []{1,2,3,5,6,7,8},
+                    new []{"a", "d", "a"});
+            }
+        }
+
+
+        [Test, TestCaseSource(nameof(BinarySearch_PositiveData))]
+        public int BinarySearch_PositiveTests(dynamic arr, dynamic value)//?
+        {
+            return BinarySearch(arr, value);
+        }
+
+        [Test, TestCaseSource(nameof(BinarySearch_ThrowsRuntimeBinderExceptionData))]
+        public void BinarySearch_ThrowsRuntimeBinderException(dynamic arr, dynamic value)
+        {
+            Assert.Throws<RuntimeBinderException>(() => BinarySearch(arr, value));
+        }
+    }
+}
---
+++ BinarySearchLogic/GenericBinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchLogic
+{
+    public static class GenericBinarySearch
+    {
+        public static int BinarySearch<T>(T[] arr, T value)
+        {
+            return BinarySearch(0, arr.Length, arr, value);
+        }
+
+        private static int BinarySearch(int bottom, int top, dynamic arr, dynamic value)
+        {
+            if (bottom > top)
+                return -1;
+
+            int mid_index = (top + bottom) / 2;
+
+            if (value.CompareTo(arr[mid_index]) < 0)
+                return BinarySearch(bottom, mid_index - 1, arr, value);
+            if (value.CompareTo(arr[mid_index]) > 0)
+                return BinarySearch(mid_index + 1, top, arr, value);
+            return mid_index;
+        }
+    }
+}
